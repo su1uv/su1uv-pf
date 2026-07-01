e2aa0ea28f099a36f37c598a4db1e0f1e4bc1ffd
su1uv <ortizemilio.dev@gmail.com>
2026-07-01 14:46:13 -0600

    feat(html): markdown to html feat and tests (imgs not supported yet)

---
+++ src/markdown_to_html.py
@@ -0,0 +1,78 @@
+from block_markdown import BlockType, block_to_block_type, markdown_to_blocks
+from htmlnode import HTMLNode, ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import TextNode, TextType, text_node_to_html_node
+
+
+def markdown_to_html_node(markdown: str) -> ParentNode:
+    blocks: list[str] = markdown_to_blocks(markdown)
+    html_children: list[HTMLNode] = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        tag, children = block_to_html(block_type, block)
+        html_children.append(ParentNode(tag, children=children))
+    html: ParentNode = ParentNode("div", html_children)
+    return html
+
+
+def block_to_html(block_type: BlockType, block: str) -> tuple[str, list[HTMLNode]]:
+    tag: str = ""
+    text: str = ""
+    children: list[HTMLNode] = []
+    grandchildren: list[HTMLNode] = []
+    match block_type:
+        case BlockType.HEADER:
+            tag = header_to_tag(block)
+            text = block.lstrip("# ")
+            grandchildren = text_to_children(text)
+            return (tag, grandchildren)
+        case BlockType.QUOTE:
+            tag = "blockquote"
+            text = block.lstrip("> ").replace("\n", " ")
+            grandchildren = text_to_children(text)
+            return (tag, grandchildren)
+        case BlockType.UNORDERED_LIST:
+            tag = "ul"
+            text_lines = block.split("\n")
+            for line in text_lines:
+                line = line.lstrip("- ")
+                grandchildren = text_to_children(line)
+                children.append(ParentNode("li", children=grandchildren))
+            return (tag, children)
+        case BlockType.ORDERED_LIST:
+            tag = "ol"
+            text_lines = block.split("\n")
+            for line in text_lines:
+                line = line.lstrip("1234567890. ")
+                grandchildren = text_to_children(line)
+                children.append(ParentNode("li", children=grandchildren))
+            return (tag, children)
+        case BlockType.CODE:
+            tag = "pre"
+            text = block.lstrip("```\n").rstrip("```")
+            node = TextNode(text, TextType.PLAIN_TEXT)
+            leaf_node = text_node_to_html_node(node)
+            grandchildren.append(leaf_node)
+            children.append(ParentNode("code", children=grandchildren))
+            return (tag, children)
+        case _:
+            tag = "p"
+            text = block.replace("\n", " ")
+            grandchildren = text_to_children(text)
+            return (tag, grandchildren)
+
+
+def text_to_children(text: str) -> list[HTMLNode]:
+    children: list[HTMLNode] = []
+    nodes: list[TextNode] = text_to_textnodes(text)
+    for node in nodes:
+        children.append(text_node_to_html_node(node))
+
+    return children
+
+
+def header_to_tag(block: str) -> str:
+    for i, char in enumerate(block):
+        if char != "#":
+            return f"h{i}"
+    return "h1"
---
+++ src/test_markdown_to_html.py
@@ -0,0 +1,110 @@
+import unittest
+
+from markdown_to_html import markdown_to_html_node
+
+
+class TestMarkdownToHTML(unittest.TestCase):
+    def test_headers(self):
+        md = """
+# This is a 1 header
+
+## This is a 2 header
+
+### This is a 3 header
+
+#### This is a 4 header
+
+##### This is a 5 header
+
+###### This is a 6 header
+
+this is a paragraph with **bold** text with
+some inline stuff and _italic_ stuff
+        """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>This is a 1 header</h1><h2>This is a 2 header</h2><h3>This is a 3 header</h3><h4>This is a 4 header</h4><h5>This is a 5 header</h5><h6>This is a 6 header</h6><p>this is a paragraph with <b>bold</b> text with some inline stuff and <i>italic</i> stuff</p></div>",
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_lists(self):
+        md = """
+- This is a list item
+- This is another list item
+
+1. Here an ordered list item
+2. Here another ordered list item
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list item</li><li>This is another list item</li></ul><ol><li>Here an ordered list item</li><li>Here another ordered list item</li></ol></div>",
+        )
+
+    def test_paragraphs_with_link(self):
+        md = """
+This is a [link](https://www.example.com)
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            '<div><p>This is a <a href="https://www.example.com">link</a></p></div>',
+        )
+
+
+# TODO: Handle images, auto closed tags
+
+#     def test_paragraphs_with_img(self):
+#         md = """
+# This is a ![image](https://www.example.com/image.png)
+#     """
+
+#         node = markdown_to_html_node(md)
+#         print(node)
+#         html = node.to_html()
+#         self.assertEqual(
+#             html,
+#             '<div><p>This is a<img src="https://www.example.com/image.png" alt="image"></p></div>',
+#         )
+
+
+if __name__ == "__main__":
+    unittest.main()
